69158dd1ec745a05a9cf3fb2f16a999186f4aa70
Rathin7 <rathinbaraiRB637@gmail.com>
2023-03-28 03:33:56 +0600

    Save cart to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 
 import { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,9 +14,14 @@ const Shop = () => {
             .then(data => setProducts(data))
         
     }, [])
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    },[])
     const handelAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
 
     }
     return (
